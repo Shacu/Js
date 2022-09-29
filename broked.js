0819fb2085a86093e2ff65e8dc8a93de6a943c5d
Shacu <nicolasdoga62@gmail.com>
2022-09-29 17:03:30 -0300

    subida segunda entrega desafio final

--- broked.js
+++ broked.js
@@ -1,35 +1,122 @@
 class Ropa{
-    constructor (nombre, precio){
+    constructor (nombre, precio, id, img, cantidad){
         this.nombre = nombre;
         this.precio = precio;
+        this.id = id;
+        this.img = img;
+        this.cantidad = cantidad;
     }
 }
 
-const buzoUno = new Ropa ("buzo uno", 5000);
-const buzoDos = new Ropa ("buzo dos", 5500);
-const pantalonCargo = new Ropa ("pantalon cargo", 7000)
-const remeraUno = new Ropa ("remera uno", 3000)
-const remeraDos = new Ropa ("remera dos", 3500)
+const buzoUno = new Ropa ("buzo uno", 5000, 1, "./img/buzo.jpg", 1);
+const buzoDos = new Ropa ("buzo dos", 5500, 2, "./img/buzo.jpg", 1);
+const pantalonCargo = new Ropa ("pantalon cargo", 7000, 3, "./img/cargo.jpg", 1)
+const remeraUno = new Ropa ("remera uno", 3000, 4, "./img/remera.jpg", 1)
+const remeraDos = new Ropa ("remera dos", 3500, 5, "./img/remera.jpg", 1)
 
 const prendas = [buzoUno, buzoDos, pantalonCargo, remeraUno, remeraDos]
 
-const carrito = []
+let carrito = []
 
 const cardPrendas = document.getElementById("cardPrendas");
+const carritoHtml = document.getElementById("carritoHtml");
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+const precioTotal = document.getElementById("precioTotal");
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("1234")){
+        carrito = JSON.parse(localStorage.getItem("1234"))
+        actualizarCarrito()
+    }
+})
 
 prendas.forEach( ropa => {
     let card = document.createElement("card");
-    card.innerHTML = ` <div class="card" style="width: 18rem;">
-                            <img src="..." class="card-img-top" alt="...">
+    card.innerHTML = ` <div class="card col-xl-3" style="width: 18rem;">
+                            <img src=" ${ropa.img}" class="card-img-top" alt="...">
                             <div class="card-body">
                             <h5 class="card-title"> ${ropa.nombre} </h5>
                             <p class="card-text"> Esta ropa es muy comoda, muy barata y you now :) tan solo por $ ${ropa.precio} </p>
-                            <a href="#" class="btn btn-primary"> Compra aca! </a>
+                            <button id= "agregar${ropa.id}" class="btn btn-primary"> Compra aca! </button>
                             </div>
                         </div> `;
 
     cardPrendas.appendChild(card);
+
+    const boton = document.getElementById(`agregar${ropa.id}`)
+
+    boton.addEventListener(`click`, () => {
+        agregarAlCarrito(ropa.id)
+    })
 })
+
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some (prod => prod.id === prodId);
+
+    if (existe){
+        const prod = carrito.map (prod => {
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    }else {
+
+    const item = prendas.find((prod) => prod.id === prodId)
+    carrito.push(item)
+}
+    actualizarCarrito()
+    console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+    carritoHtml.innerHTML = ""
+    carrito.forEach((ropa) => {
+        let div = document.createElement("div");
+        div.innerHTML = ` <div class="card col-4" style="width: 18rem;">
+                            <img src=" ${ropa.img}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title"> ${ropa.nombre} </h5>
+                            <p class="card-text"> Esta ropa es muy comoda, muy barata y you now :) tan solo por $ ${ropa.precio} </p>
+                            <p> ${ropa.cantidad} </p>
+                            <button onClick="eliminarDelCarrito( ${ropa.id} )" class="btn btn-primary"> Eliminar prenda </button>
+                            </div>
+                        </div> `;
+
+        carritoHtml.appendChild(div);
+
+        localStorage.setItem("1234", JSON.stringify(carrito));
+    })
+    calcularTotalCompra();
+   /*  guardadoStorage(); */
+}
+
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+    carrito.splice(carrito.indexOf(producto,1));
+    actualizarCarrito();
+}
+
+vaciarCarrito.addEventListener("click", () => {
+    carrito.splice(0, carrito.length);
+    actualizarCarrito();
+});
+
+const calcularTotalCompra = () => {
+    let total = 0
+    carrito.forEach(producto => {
+        total += producto.precio * producto.cantidad
+    });
+    precioTotal.innerHTML = total;
+};
+
+/* const guardadoStorage = () => {
+    carrito.forEach(producto => {
+        productoJSON = JSON.stringify(producto);
+    })
+    localStorage.setItem("1234", productoJSON);
+    console.log(productoJSON);
+};
+console.log(guardadoStorage); */
 
 
 alert("Buenos dias! Bienvenido a nuestra tienda online");
@@ -38,7 +125,7 @@ let nombreUsuario = prompt("ingrese su nombre: ");
 
 console.log("Bienvenido " + nombreUsuario + ", esta es una lista de todos nuestros productos")
 
-for(let i = 0; i < prendas.length; i++ ){
+/* for(let i = 0; i < prendas.length; i++ ){
     console.log(prendas[i]);
 }
 
@@ -83,7 +170,7 @@ if(sumarCarrito === "buzo uno" || sumarCarrito === "buzo dos" || sumarCarrito ==
             break
     }
 }
-console.log(carrito)
+console.log(carrito) */
 
 const formDatos = document.getElementById("formDatos");
 
